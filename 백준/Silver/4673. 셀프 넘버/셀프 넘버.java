d9e62915e45154612a14d50e64b19227da2f862d
sangehee-ju <72913881+sangehee-ju@users.noreply.github.com>
2022-03-14 14:44:57 +0900

    [Silver V] Title: 셀프 넘버, Time: 120 ms, Memory: 13848 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,34 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean[] check = new boolean[10001]; 
+		for(int i=0;i<10001;i++) {
+			int n = d(i);
+			
+			if(n<10001) {
+				check[n] = true;
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0;i<10001;i++) {
+			if(!check[i]) {
+				sb.append(i).append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
+	
+	public static int d(int num) {
+		
+		int sum = num;
+		
+		while(num!=0) {
+			sum = sum + (num%10);
+			num = num/10;
+		}
+		return sum;
+	}
+}
