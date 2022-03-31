6a4d8c5241b9b44ee9a8ab85609d263920559e39
sanghee-ju <72913881+sanghee-ju@users.noreply.github.com>
2022-03-31 17:24:45 +0900

    [Silver I] Title: 이항 계수 2, Time: 76 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,17 @@
+import sys
+
+r = sys.stdin.readline
+
+N, K = map(int, r().split())
+res = 1
+
+if K > (N / 2):
+    K = N - K
+
+for i in range(N, (N-K), -1):
+    res *= i
+
+for i in range(K, 0, -1):
+    res //= i
+
+print(res % 10007)
