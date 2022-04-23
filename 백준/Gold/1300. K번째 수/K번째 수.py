08f37b15face9513fbb22a67e375e12b710449bc
sanghee-ju <72913881+sanghee-ju@users.noreply.github.com>
2022-04-23 22:09:02 +0900

    [Gold II] Title: K번째 수, Time: 1024 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,24 @@
+import sys
+
+r = sys.stdin.readline
+
+n = int(r())
+k = int(r())
+
+start, end = 1, k
+answer = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    temp = 0
+
+    for i in range(1, n + 1):
+        temp += min(mid // i, n)
+
+    if temp >= k:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(answer)
